5881e6e811e6e21dd0397b9e3fb7adc2c5ec63c2
kathyayinikam <145150034+kathyayinikam@users.noreply.github.com>
2023-11-02 18:34:43 +0530

    Add files via upload

---
+++ kk1.java
@@ -0,0 +1,22 @@
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
+
+
+public class kk1 {
+    
+        public static void main(String[]args){
+            rule3 ob=new rule3();
+        List<String>mylist=new ArrayList<>();
+        mylist.add("apple");
+        mylist.add("banana");
+        mylist.add("carrot");
+        mylist.add("drumstick");
+      String result=ob.rule3(mylist);
+      System.out.println("player has entered the word"+" "+result);
+}
+}
+
+
+        
